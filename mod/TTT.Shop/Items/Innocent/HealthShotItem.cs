da1178193cfc6c74ba09e91ba6931d5e0ff6ecc5
dryden <swiggynmbr1@gmail.com>
2024-04-14 14:41:40 -0300

    added heathshot item

---
+++ mod/TTT.Shop/Items/Innocent/HealthShotItem.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Security;
+using CounterStrikeSharp.API.Modules.Entities.Constants;
+using TTT.Player;
+using TTT.Public.Shop;
+
+namespace TTT.Shop.Items.Innocent;
+
+public class HealthShotItem : IShopItem
+{
+    public string Name()
+    {
+        return "Health Shot";
+    }
+
+    public string SimpleName()
+    {
+        return "HealthShot";
+    }
+
+    public int Price()
+    {
+        return 250;
+    }
+
+    public BuyResult OnBuy(GamePlayer player)
+    {
+        if (player.Credits() < Price())
+            return BuyResult.NotEnoughCredits;
+        player.Player().GiveNamedItem(CsItem.Healthshot);
+        return BuyResult.Successful;
+    }
+}
